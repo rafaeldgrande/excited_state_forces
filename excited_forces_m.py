172ff4977c838c55b2f2938fa96ca1a4b96250b2
rafaeldgrande <rafaeldgrande@gmail.com>
2022-04-12 17:19:37 -0700

    Rewrote the functions that reads the elph matrix elements. Now it is more organized and seems to be correct!

--- excited_forces_m.py
+++ excited_forces_m.py
@@ -140,7 +140,6 @@ def get_hdf5_exciton_info(exciton_file, iexc):
     TODO -> for now calculting exciton info for exciton with index iexc
     but later, make it calculate for and set of exciton indexes"""
 
-
     print('Reading exciton info from file', exciton_file)
 
     f_hdf5 = h5py.File(exciton_file, 'r')
@@ -164,7 +163,6 @@ def get_patterns2(el_ph_dir, iq, Nmodes, Nat):
 
     Displacements = np.zeros((Nmodes, Nat, 3), dtype=np.complex64)
     imode = 0
-    Perts = []
     
     patterns_file = el_ph_dir+'patterns.'+str(iq + 1)+'.xml'
     
@@ -192,7 +190,6 @@ def get_patterns2(el_ph_dir, iq, Nmodes, Nat):
 
         Npert = int(texts_in_xml_file[Npert_indexes_in_tag[irrep]])
         print('Npert =', Npert)
-        Perts.append(Npert)
 
         for ipert in range(Npert):
             idisp += 1
@@ -212,260 +209,164 @@ def get_patterns2(el_ph_dir, iq, Nmodes, Nat):
 
             imode += 1
 
-    return Displacements, Nirreps, Perts
+    return Displacements, Nirreps
 
 
+def read_elph_xml(elph_xml_file):
 
+    """Reads elph coefficients (<i_k|dV/dx_mu|j_(k+q)>) produced by DFPT calculations from Quantum Espresso
+    Those coefficients are written in .xml files. The nomenclature of those files are
+    elph.iq.ipert.xml, where iq = 1,2,3... is the index of the q point that calculation and
+    ipert is the index of the perturbation applied for that mode. The displacements patterns 
+    for each perturbation are listed in the patterns.iq.xml files.
 
-def get_patterns(el_ph_dir, iq, Nmodes, Nat):  # suitable for xml written from qe 6.7 
-
-    """Old function that reads displacement patterns. To be deleted later"""
-
-    Displacements = np.zeros((Nmodes, Nat, 3), dtype=np.complex64)
-    imode = 0
-    Perts = []
-    
-    patterns_file = el_ph_dir+'patterns.'+str(iq + 1)+'.xml'
-    arq = open(patterns_file)
-
-    for i in range(6):  # do not care about this info now
-        arq.readline()
-
-    line = arq.readline()  # <NUMBER_IRR_REP>XX</NUMBER_IRR_REP>
-    Nirreps = int(line.split('<')[1].split('>')[1])
-
-    for i_irreps in range(Nirreps):
-        arq.readline()  # <REPRESENTION.X>
-
-        line = arq.readline() # <NUMBER_OF_PERTURBATIONS>1</NUMBER_OF_PERTURBATIONS>
-
-        Npert = int(line.split('<')[1].split('>')[1])
-        Perts.append(Npert)
-
-        for ipert in range(Npert):
-
-            arq.readline() # <PERTURBATION.1>
-            arq.readline() # <DISPLACEMENT_PATTERN>
-
-            for iat in range(Nat):  # reading 3*Nat displacements - it is a complex number
-                for idir in range(3):
-                    line = arq.readline().split()  
-                    Displacements[imode][iat][idir] = float(line[0]) + 1.0j*float(line[1])
-
-            arq.readline() # </DISPLACEMENT_PATTERN>
-            arq.readline() # </PERTURBATION.1>
-
-            imode += 1
-
-        arq.readline()  # </REPRESENTION.1>
-
-    arq.close()
-
-    return Displacements, Nirreps, Perts        
-
-def get_el_ph_coeffs2(el_ph_dir, iq, Nirreps, params_calc, Perts):  # suitable for xml written from qe 6.7 
-
-    """Read elph coefficients from elph files. Has to organize them. Data in those files are printed as
-    
-    ...
-    -1.595702686385000E-001,-1.006139616066548E-016  # for bands 1 1 
-    -8.573884170996401E-009,-6.353890362313132E-009  # for bands 1 2 
-    -4.916126043766308E-002, 3.098379902211784E-003  # for bands 1 3
-    -8.573884179886859E-009, 6.353890313686664E-009  # for bands 2 1 
-    2.953855120010117E-001, 0.000000000000000E+000   # for bands 2 2 
-    ...
-
-    where i, j runs from 1 to (Ncbnds + Nvbnds)
-
-    Needs to translate to ic and iv. Exs:
-
-    i = 1,2,3,4,5,6,7
-
-    And Nval = 3, then
-
-    i   iv
-    1   3
-    2   2
-    3   1
-
-    i   ic
-    4   1
-    5   2
-    6   3
-    7   4
-
-    So iv = Nval - i + 1 and ic = i - Nval
-    
+    Returns:
+        elph_aux = [Ndeg, Nkpoints_in_xml_file, Nbnds_in_xml_file, Nbnds_in_xml_file]
+        complex array
+        where: 
+            - Ndeg is the degeneracy of this mode
+            - Nkpoints_in_xml is the number of k points in the file
+            - Nbnds_in_xml_file is the number of bands in the file 
     """
+
+    print('Reading file ', elph_xml_file)
+
+    tree = ET.parse(elph_xml_file)
+    root = tree.getroot()
+
+    # get tags in xml file (ex: NUMBER_OF_BANDS)
+    tags_in_xml_file = [elem.tag for elem in root.iter()]
+    # get text in xml file for each element. Those are the info we want to read
+    texts_in_xml_file = [elem.text for elem in root.iter()]
+
+    # TODO -> just reads 1 k point until now. Needs to be generalized for more q points later
+
+    # reading number of bands in xml file
+    Nbnds_index_in_tag = tags_in_xml_file.index('NUMBER_OF_BANDS')
+    Nbnds_in_xml_file = int(texts_in_xml_file[Nbnds_index_in_tag])
+    print(f'Number of bands in this file {Nbnds_in_xml_file}')
+
+    # reading number of k points in xml file
+    Nkpoints_index_in_tag = tags_in_xml_file.index('NUMBER_OF_K')
+    Nkpoints_in_xml_file = int(texts_in_xml_file[Nkpoints_index_in_tag])
+    print(f'Number of k points in this file {Nkpoints_in_xml_file}')
+
+    # reading elph matrix elements. 
+    elph_mat_elems_index = tags_in_xml_file.index('PARTIAL_ELPH')
+    text_temp = texts_in_xml_file[elph_mat_elems_index]
+    # this text is something like this
+    # ...
+    # 1.1,2.2
+    # -3.3,4.4
+    # -5.5,6.6
+    # ...
+    text_temp = text_temp.replace(",", " ")  # replace "," per spaces 
+    numbers_temp = np.fromstring(text_temp, sep='\n')  # transforming text to floats
+
+    # Now variable is an 1D array like this
+    # ... 1.1 2.2 3.3 4.4 5.5 6.6 ... (same numbers from previous comment)
+    # In this array, elements with odd (even) index are the real (imaginary) part 
+    # of a complex number. 
+    temp_elph = numbers_temp[::2] + 1.0j*numbers_temp[1::2]
+
+    # Now get degeneracy of this mode
+    # Number of matrix elements = (Degeneracy of mode) * (Number of bands)**2 
+    Ndeg = int(len(temp_elph) / Nbnds_in_xml_file**2)
+    print(f'Number of modes in this file is {Ndeg}')
+
+    # Building elph matrix
+    elph_aux = np.zeros((Ndeg, Nkpoints_in_xml_file, Nbnds_in_xml_file, Nbnds_in_xml_file), dtype=np.complex64)
+
+    contador = 0
+    for ideg in range(Ndeg):
+        for ibnd in range(Nbnds_in_xml_file):
+            for jbnd in range(Nbnds_in_xml_file):
+                elph_aux[ideg, 0, ibnd, jbnd] = temp_elph[contador]
+                contador += 1
+
+    return elph_aux
+
+
+def get_el_ph_coeffs(el_ph_dir, iq, Nirreps):  # suitable for xml files written from qe 6.7 
+
+    """ Reads all elph.iq.ipert.xml files and returns the electron-phonon coefficients
+    elph[Nmodes, Nk, Nbnds_in_xml, Nbnds_in_xml] """
 
     print('\n\nReading elph coeficients g_ij = <i|dH/dr|j>\n')
 
+    elph = []
+
+    for irrep in range(Nirreps):
+        elph_xml_file = el_ph_dir + f'elph.{iq + 1}.{irrep + 1}.xml'
+        elph_aux = read_elph_xml(elph_xml_file)
+
+        for ideg in range(len(elph_aux)):
+            elph.append(elph_aux[ideg])
+
+    elph = np.array(elph)
+
+    return elph
+
+def filter_elph_coeffs(elph, params_calc):
+
+    """ Reads elph coefficients from DFPT calculations. Quantum Espresso calculates <i|dV/dx_mu|j> for 
+    i, j = 1,2,3,...,Nbnds_in_xml, where Nbnds_in_xml = total of bands included in the scf calculation step before DFPT.
+    We just need <c|dV/dx_mu|c'> and <v|dV/dx_mu|v'>, where c ranges from Nval + 1 to Nval + Ncbnds (conduction bands) and
+    v ranges from (Nval - Nvbnds + 1) to Nval. In other words, we just need two blocks from the elph matrix.
+    
+    Other important information, is that the indexes need to be updated as the counting used in the code is different 
+    from the one used QE. Conduction bands start been counted from Nval + 1 as 1 upwards and valence bands are counted
+    from Nval as 1 downwards. An example is the following:
+
+    indexQE    iv    ic
+    1          4
+    2          3
+    3          2
+    4          1
+    5                 1
+    6                 2
+    7                 3
+
+    where the highest valence band (Nval) is 4. The rule to update the indexes are
+    iv = Nval - iQE + 1
+    ic = iQE - Nval
+    """
+
     Nkpoints, Ncbnds, Nvbnds, Nval, Nmodes = params_calc
 
-    Nbnds = Nvbnds + Ncbnds
     elph_cond = np.zeros((Nmodes, Nkpoints, Ncbnds, Ncbnds), dtype=np.complex64)
     elph_val = np.zeros((Nmodes, Nkpoints, Nvbnds, Nvbnds), dtype=np.complex64)
 
-    imode = 0 # contador de modos
+    Nbnds_in_xml_file = np.shape(elph)[2]
+    Ncond_in_xml_file = Nbnds_in_xml_file - Nval
 
-    for i_irrep in range(Nirreps):
+    if Ncond_in_xml_file < Ncbnds:
+        print(f'Missing {Ncbnds - Ncond_in_xml_file} cond bands from DFPT calculations. Missing coefficients will be set to 0.')
 
-        el_ph_coeffs_file = el_ph_dir+'elph.'+str(iq + 1)+'.'+str(i_irrep+1)+'.xml'
-        print('Reading: ', el_ph_coeffs_file)
+    for imode in range(Nmodes):
+        for ik in range(Nkpoints):
 
-        tree = ET.parse(el_ph_coeffs_file)
-        root = tree.getroot()
+            Ncbnds_to_get = min(Ncbnds, Ncond_in_xml_file)
+            Nmin = Nval 
+            Nmax = Nval + Ncbnds_to_get 
+            elph_cond[imode, ik] = elph[imode, ik, Nmin:Nmax, Nmin:Nmax]
 
-        # get tags in xml file (ex: NUMBER_OF_BANDS)
-        tags_in_xml_file = [elem.tag for elem in root.iter()]
-        # get text in xml file for each element. Those are the info we want to read
-        texts_in_xml_file = [elem.text for elem in root.iter()]
+            Nvbnds_to_get = min(Nvbnds, Nval)
+            Nmin = Nval - Nvbnds_to_get
+            Nmax = Nval 
+            temp = elph[imode, ik, Nmin:Nmax, Nmin:Nmax]
 
-
-        # How many bnds in xml files? Just need to check it once (assuming that user won't use files from different DFPT calculations here)
-        if i_irrep == 0:
-
-            Nbnds_index_in_tag = tags_in_xml_file.index('NUMBER_OF_BANDS')
-            Nbnds_in_xml_file = int(texts_in_xml_file[Nbnds_index_in_tag])
-            #Nbnds_in_xml_file = int(root[1][1].text)
-            print(f'There are {Nbnds_in_xml_file} bands in those elph files')
-
-            # para checagem
-            elph_aux = np.zeros((Nmodes, Nkpoints, Nbnds_in_xml_file, Nbnds_in_xml_file), dtype=np.complex64)
-
-            if Nbnds > Nbnds_in_xml_file:
-                print('WARNING! Number of bands in elph files is not enough!')
-                print('Setting missing elph coefficients to 0. '+ str(Nbnds - Nbnds_in_xml_file)+' bands are missing')
-
-
-        Npert = Perts[i_irrep]
-        print(f'Number of modes (perturbations) in this file: {Npert}')
-
-        # just appears one time for one k pointt. TODO -> later generalize for several k points!
-        elph_index_in_xml_file = indexes_x_in_list('PARTIAL_ELPH', tags_in_xml_file)[0] 
-
-        for ik in range(Nkpoints):    
-            #text_temp = root[1][2 + ik][ipert + 1].text
-            # TODO still dumb for just one k point! later extend it to read more k points
-            text_temp = texts_in_xml_file[elph_index_in_xml_file]
-            print(text_temp.count(','))
-            text_temp = text_temp.replace(",", " ")
-            numbers_temp = np.fromstring(text_temp, sep='\n')
-            # reading complex numbers -> A[::2] (A[1::2]) gives the first (second) collum
-            temp_elph = numbers_temp[::2] + 1.0j*numbers_temp[1::2]
-            # size of this list is Npert*Nbnds*Nbnds. Must divide this data!
-
-            icounter = 0
-
-            for ipert in range(Npert):
-                for ibnd in range(Nbnds_in_xml_file):
-                    for jbnd in range(Nbnds_in_xml_file):
-
-                        elph_aux[imode, ik, ibnd - Nval, jbnd - Nval] = temp_elph[icounter]
-
-                        if do_I_want_this_band(ibnd, Nval, Nvbnds, 'v') == True and do_I_want_this_band(jbnd, Nval, Nvbnds, 'v') == True:
-                            ibndp, jbndp = Nval - ibnd, Nval - jbnd
-                            elph_val[imode, ik, ibndp, jbndp] = temp_elph[icounter]
-                        
-                        if do_I_want_this_band(ibnd, Nval, Ncbnds, 'c') == True and do_I_want_this_band(jbnd, Nval, Ncbnds, 'c') == True:
-                            ibndp, jbndp = Nval - ibnd, Nval - jbnd
-                            elph_cond[imode, ik, ibndp, jbndp] = temp_elph[icounter]
-                        icounter += 1
-                imode += 1
+            # Making a offdiagonal transpose 
+            tuple_iteration = range(-1, -(Nvbnds_to_get+1), -1)  
+            elph_val[imode, ik] = np.array([[temp[iv, jv] for iv in tuple_iteration] for jv in tuple_iteration])
 
     # small report
     print(f"\nMax real value of <c|dH|c'> (eV/A): {np.max(np.real(elph_cond))}")
     print(f"Max imag value of <c|dH|c'> (eV/A): {np.max(np.imag(elph_cond))}")
     print(f"Max real value of <v|dH|v'> (eV/A): {np.max(np.real(elph_val))}")
     print(f"Max imag value of <v|dH|v'> (eV/A): {np.max(np.imag(elph_val))}")
-    
-    return elph_aux, elph_cond, elph_val
 
-
-def get_el_ph_coeffs(el_ph_dir, iq, Nirreps, Perts, Nmodes, Nkpoints, Ncbnds, Nvbnds, Nval):  # suitable for xml written from qe 6.7 
-
-    """
-    Old -> to be deleted later
-    # FIXME -> rewrite it to read .xml files using .xml modules in python
-    # total of el_ph files is Nq*Nirreps and 
-    # nomenclature is elph."iq"."i_irrep".xml with 
-    # 1 <= iq <= Nq and 1 <= i_irrep <= Nirreps
-    # Here I will read all irreps for a given iq
-    """
-
-    Nbnds = Nvbnds + Ncbnds
-
-    elph_cond = np.zeros((Nmodes, Nkpoints, Ncbnds, Ncbnds), dtype=np.complex64)
-    elph_val = np.zeros((Nmodes, Nkpoints, Nvbnds, Nvbnds), dtype=np.complex64) 
-    elph_aux = []
-    # I just want <c'k|dV/du_{mode, q=0}|ck>  and 
-    # <v'k|dV/du_{mode, q=0}|vk>, so I will read the file 
-    # entirely and then filter to return those quantities
-
-    imode = -1 # contador de modos
-
-    for i_irrep in range(Nirreps):
-
-        el_ph_coeffs_file = el_ph_dir+'elph.'+str(iq + 1)+'.'+str(i_irrep+1)+'.xml'
-        print('Reading: ', el_ph_coeffs_file)
-        
-
-        arq = open(el_ph_coeffs_file)
-
-        for i in range(6):  # not important for me
-            arq.readline()
-
-        # <NUMBER_OF_BANDS>31</NUMBER_OF_BANDS>
-        temp_line = arq.readline()
-        temp_Nbnds = int(temp_line.split("<")[1].split(">")[1])
-        print('Number of bands in .xml file '+str(temp_Nbnds))
-        elph_aux_temp = np.zeros((Nkpoints, temp_Nbnds, temp_Nbnds), dtype=np.complex64)
-
-        for ik in range(Nkpoints):
-
-            arq.readline() # <K_POINT.1>
-
-            for i in range(3): # not important for me
-                arq.readline()
-
-            for ipert in range(Perts[i_irrep]):
-                imode += 1
-                print('    Now in mode ', imode + 1)
-                arq.readline()  # <PARTIAL_ELPH perturbation="1">
-
-                for iband in range(temp_Nbnds):
-                    for jband in range(temp_Nbnds):
-                        line = arq.readline().split()
-                        #print(iband, jband, line)
-                        elph_aux_temp[ik][iband][jband] = float(line[0]) + 1.0j*float(line[1])
-
-                arq.readline()  # </PARTIAL_ELPH>
-                elph_aux.append(elph_aux_temp)
-
-            arq.readline() # </K_POINT.1>
-        
-        arq.close()
-
-    # convert from list to array
-    elph_aux = np.array(elph_aux)
-    # filtering data
-
-    for imode in range(Nmodes):
-        for ik in range(Nkpoints):
-
-            for iband_new in range(Ncbnds):
-                iband = iband_new + Nval
-                for jband_new in range(Ncbnds):
-                    jband = jband_new + Nval
-                    elph_cond[imode][ik][iband_new][jband_new] = elph_aux[imode][ik][iband][jband]
-
-            for iband_new in range(Nvbnds):
-                iband = Nval - 1 - iband_new
-                for jband_new in range(Nvbnds):
-                    jband = Nval - 1 - jband_new
-                    elph_val[imode][ik][iband_new][jband_new] = elph_aux[imode][ik][iband][jband]
-
-    return elph_val, elph_cond, elph_aux
+    return elph_cond, elph_val
 
 def get_modes2cart_matrix(dyn_file, Nat, Nmodes):
     # Read eigenvecs - FIXME -> generalize for several q's
@@ -557,6 +458,7 @@ def calculate_Fcvk(Ncbnds, Nvbnds, Akcv, Edft_cond, Edft_val, Eqp_cond, Eqp_val,
 
     """ calculate force matrix elements
     Old -> to be deleted later"""
+
     Fcvk_offdiag = 0.0 + 1.0j*0.0
     Fcvk_diag = 0.0 + 1.0j*0.0
 
@@ -590,6 +492,7 @@ def aux_matrix_elem(Nmodes, Nkpoints, Ncbnds, Nvbnds, elph_cond, elph_val, Edft_
     aux_cond_matrix[imode, ik, ic1, ic2] = elph_cond[imode, ik, ic1, ic2] * deltaEqp / deltaEdft (if ic1 != ic2)
     aux_val_matrix[imode, ik, iv1, iv2]  = elph_val[imode, ik, iv1, iv2]  * deltaEqp / deltaEdft (if iv1 != iv2)
     If ic1 == ic2 (iv1 == iv2), then the matrix elements are just the elph coefficients"""
+
     Shape_cond = (Nmodes, Nkpoints, Ncbnds, Ncbnds)
     aux_cond_matrix = np.zeros(Shape_cond, dtype=np.complex64)
 
