c7ce06544b0f4cc293dc39c8a98913111b8afc22
rafaeldgrande <rafaeldgrande@gmail.com>
2022-09-19 00:37:28 -0700

    Now the code can calculate offdiag exciton-phonon matrix elements <A|dH/dr|B>. It still just calculates it in the cartesian basis. I intend to calculate in the phonon basis in the future, but in a post process step

--- excited_forces_config.py
+++ excited_forces_config.py
@@ -1,4 +1,5 @@
 
+from scipy.constants import physical_constants
 import numpy as np
 
 # File with variables used in excited_forces.py code
@@ -11,7 +12,6 @@ import numpy as np
 TOL_DEG = 1e-5  # tolerance to see if two energy values are degenerate  ----------
 
 # Conversion factors
-from scipy.constants import physical_constants
 Ry2eV = physical_constants["Rydberg constant times hc in eV"][0]
 bohr2A = physical_constants["Bohr radius"][0]*1e10
 
@@ -20,32 +20,36 @@ bohr2A = physical_constants["Bohr radius"][0]*1e10
 
 # Dumb default parameters
 iexc = 1
+jexc = -1  # if it keeps to be jexc, then make jexc to iexc
 factor_head = 1
-# # files and paths to be opened 
+# # files and paths to be opened
 eqp_file = 'eqp1.dat'
 exciton_file = 'eigenvectors.h5'
 el_ph_dir = './'
 kernel_file = 'bsemat.h5'
 
 # conditionals
-calc_modes_basis = False    # not being used yet 
+calc_modes_basis = False    # not being used yet
 
-calc_IBL_way     = False    # I should comment later
-write_DKernel    = False    # not being used  yet
+calc_IBL_way = False    # I should comment later
+write_DKernel = False    # not being used  yet
 Calculate_Kernel = False    # Dont change. We dont know how to work with kernel yet
 
-just_RPA_diag    = False    # If true doesn't calculate forces a la David
-report_RPA_data  = False    # report Fcvkc'v'k' matrix elements.
+just_RPA_diag = False    # If true doesn't calculate forces a la David
+report_RPA_data = False    # report Fcvkc'v'k' matrix elements.
 
-read_Akcv_trick  = False    # If false, read eigenvectors.h5 file, when BGW is compiled with hdf5. If true read Acvk{iexc} that is an output of my modified summarize_eigenvectors.f90 file
+# If false, read eigenvectors.h5 file, when BGW is compiled with hdf5. If true read Acvk{iexc} that is an output of my modified summarize_eigenvectors.f90 file
+read_Akcv_trick = False
 
-show_imag_part    = False   # Show imaginary part of excited state force . It should be very close to 0 
+# Show imaginary part of excited state force . It should be very close to 0
+show_imag_part = False
 
-acoutic_sum_rule = True     # Makes the excited state forces to sum to zero (obey Newton's third law), by making sum of elph matrix elems to 0. May want to set this variable to true
-use_hermicity_F  = True     # Use the fact that F_cvc'v' = conj(F_c'v'cv)
-                            # Reduces the number of computed terms by about half
+# Makes the excited state forces to sum to zero (obey Newton's third law), by making sum of elph matrix elems to 0. May want to set this variable to true
+acoutic_sum_rule = True
+use_hermicity_F = True     # Use the fact that F_cvc'v' = conj(F_c'v'cv)
+# Reduces the number of computed terms by about half
 
-log_k_points     = True     # Write k points used in BSE and DFPT calculations
+log_k_points = True     # Write k points used in BSE and DFPT calculations
 
 
 def true_or_false(text, default_value):
@@ -56,11 +60,12 @@ def true_or_false(text, default_value):
     else:
         return default_value
 
+
 def read_input(input_file):
 
     # getting necessary info
 
-    global iexc, factor_head
+    global iexc, jexc, factor_head
     global eqp_file, exciton_file, el_ph_dir
     global dyn_file, kernel_file
     global calc_modes_basis
@@ -85,10 +90,12 @@ def read_input(input_file):
             linha = line.split()
             if len(linha) >= 2:
 
-######## parameters and files ######################
+                ######## parameters and files ######################
 
                 if linha[0] == 'iexc':
                     iexc = int(linha[1])
+                if linha[0] == 'jexc':
+                    jexc = int(linha[1])
                 if linha[0] == 'factor_head':
                     factor_head = float(linha[1])
                 elif linha[0] == 'eqp_file':
@@ -105,11 +112,13 @@ def read_input(input_file):
 ######### conditionals #############################
 
                 elif linha[0] == 'calc_modes_basis':
-                    calc_modes_basis = true_or_false(linha[1], calc_modes_basis)
+                    calc_modes_basis = true_or_false(
+                        linha[1], calc_modes_basis)
                 elif linha[0] == 'calc_IBL_way':
                     calc_IBL_way = true_or_false(linha[1], calc_IBL_way)
                 elif linha[0] == 'Calculate_Kernel':
-                    Calculate_Kernel = true_or_false(linha[1], Calculate_Kernel)                    
+                    Calculate_Kernel = true_or_false(
+                        linha[1], Calculate_Kernel)
                 elif linha[0] == 'write_DKernel':
                     write_DKernel = true_or_false(linha[1], write_DKernel)
                 elif linha[0] == 'just_RPA_diag':
@@ -121,9 +130,10 @@ def read_input(input_file):
                 elif linha[0] == 'show_imag_part':
                     show_imag_part = true_or_false(linha[1], show_imag_part)
                 elif linha[0] == 'acoutic_sum_rule':
-                    acoutic_sum_rule = true_or_false(linha[1], acoutic_sum_rule)
+                    acoutic_sum_rule = true_or_false(
+                        linha[1], acoutic_sum_rule)
                 elif linha[0] == 'use_hermicity_F':
-                    use_hermicity_F = true_or_false(linha[1], use_hermicity_F) 
+                    use_hermicity_F = true_or_false(linha[1], use_hermicity_F)
                 elif linha[0] == 'log_k_points':
                     log_k_points = true_or_false(linha[1], log_k_points)
 
@@ -134,13 +144,18 @@ def read_input(input_file):
                     print(line)
         arq_in.close()
 
-    
+    if jexc == -1:
+        jexc = iexc
+
 
 read_input('forces.inp')
 
 print('\n--------------------Configurations----------------------------\n\n')
 
-print('Exciton index to be read : '+str(iexc))
+if jexc == iexc:
+    print(f'Exciton index to be read : {iexc}')
+else:
+    print(f'Exciton indexes to be read : {iexc}, {jexc}')
 print(f'Eqp data file : {eqp_file}')
 print(f'Exciton file : {exciton_file}')
 print(f'Elph directory : {el_ph_dir}')
--- excited_forces_m.py
+++ excited_forces_m.py
@@ -1,6 +1,5 @@
 
 
-
 # TODO -> splits this module for the different functions
 #          -> reads GW/BSE data
 #          -> reads DFPT data
@@ -16,16 +15,15 @@ from excited_forces_config import *
 
 
 def calc_DKernel_mat_elem(indexes, Kernel, EDFT, EQP, ELPH, MF_params, BSE_params):
-
     """Calculates derivatives of kernel matrix elements"""
 
     ik1, ik2, iv1, iv2, ic1, ic2, imode = indexes
 
     elph_cond, elph_val = ELPH
-    Nmodes   = MF_params.Nmodes
+    Nmodes = MF_params.Nmodes
     Nkpoints = BSE_params.Nkpoints_BSE
-    Ncbnds   = BSE_params.Ncbnds
-    Nvbnds   = BSE_params.Nvbnds
+    Ncbnds = BSE_params.Ncbnds
+    Nvbnds = BSE_params.Nvbnds
     Edft_val, Edft_cond = EDFT
 
     if calc_IBL_way == True:
@@ -33,9 +31,8 @@ def calc_DKernel_mat_elem(indexes, Kernel, EDFT, EQP, ELPH, MF_params, BSE_param
         DKelement_IBL = 0 + 0.0*1.0j
 
     DKelement = 0 + 0.0*1.0j
-    
-    # ik2, ik1, ic2, ic1, iv2, iv1
 
+    # ik2, ik1, ic2, ic1, iv2, iv1
 
     for ivp in range(Nvbnds):
 
@@ -63,7 +60,6 @@ def calc_DKernel_mat_elem(indexes, Kernel, EDFT, EQP, ELPH, MF_params, BSE_param
             if calc_IBL_way == True:
                 DKelement_IBL += Kernel[indexes_K]*elph_val[indexes_g]/DeltaEqp
 
-
     for icp in range(Ncbnds):
 
         DeltaEdft = Edft_cond[ik1, ic1] - Edft_cond[ik1, icp]
@@ -76,7 +72,8 @@ def calc_DKernel_mat_elem(indexes, Kernel, EDFT, EQP, ELPH, MF_params, BSE_param
         if abs(DeltaEdft) > TOL_DEG:
             DKelement += Kernel[indexes_K]*elph_cond[indexes_g]/DeltaEdft
             if calc_IBL_way == True:
-                DKelement_IBL += Kernel[indexes_K]*elph_cond[indexes_g]/DeltaEqp
+                DKelement_IBL += Kernel[indexes_K] * \
+                    elph_cond[indexes_g]/DeltaEqp
 
         DeltaEdft = Edft_cond[ik2, ic2] - Edft_cond[ik2, icp]
         if calc_IBL_way == True:
@@ -88,29 +85,30 @@ def calc_DKernel_mat_elem(indexes, Kernel, EDFT, EQP, ELPH, MF_params, BSE_param
         if abs(DeltaEdft) > TOL_DEG:
             DKelement += Kernel[indexes_K]*elph_cond[indexes_g]/DeltaEdft
             if calc_IBL_way == True:
-                DKelement_IBL += Kernel[indexes_K]*elph_cond[indexes_g]/DeltaEqp
+                DKelement_IBL += Kernel[indexes_K] * \
+                    elph_cond[indexes_g]/DeltaEqp
 
     if calc_IBL_way is True:
         return DKelement, DKelement_IBL
     else:
-        return DKelement   
+        return DKelement
 
 
-def calc_deriv_Kernel(KernelMat, EDFT, EQP, ELPH, Akcv, MF_params, BSE_params):
+def calc_deriv_Kernel(KernelMat, EDFT, EQP, ELPH, Akcv, Bkcv, MF_params, BSE_params):
 
     print("\n\n    - Calculating Kernel part")
 
-    Nmodes   = MF_params.Nmodes
+    Nmodes = MF_params.Nmodes
     Nkpoints = BSE_params.Nkpoints_BSE
-    Ncbnds   = BSE_params.Ncbnds
-    Nvbnds   = BSE_params.Nvbnds
+    Ncbnds = BSE_params.Ncbnds
+    Nvbnds = BSE_params.Nvbnds
 
     Shape_kernel = (Nmodes, Nkpoints, Ncbnds, Nvbnds, Nkpoints, Ncbnds, Nvbnds)
-    DKernel      = np.zeros(Shape_kernel, dtype=np.complex64)
-    DKernel_IBL  = np.zeros(Shape_kernel, dtype=np.complex64)
+    DKernel = np.zeros(Shape_kernel, dtype=np.complex64)
+    DKernel_IBL = np.zeros(Shape_kernel, dtype=np.complex64)
 
-    total_ite          = Nmodes*(Nkpoints*Ncbnds*Nvbnds)**2
-    interval_report    = max(int(np.sqrt(total_ite)), int(total_ite/20))
+    total_ite = Nmodes*(Nkpoints*Ncbnds*Nvbnds)**2
+    interval_report = max(int(np.sqrt(total_ite)), int(total_ite/20))
     i_term = 0
     print(f'        Total terms to be calculated : {total_ite}')
 
@@ -126,21 +124,25 @@ def calc_deriv_Kernel(KernelMat, EDFT, EQP, ELPH, Akcv, MF_params, BSE_params):
                         for ic2 in range(Ncbnds):
                             for iv2 in range(Nvbnds):
 
-                                A_ket = Akcv[ik2, ic2, iv2]
+                                A_ket = Bkcv[ik2, ic2, iv2]
 
                                 indexes = ik1, ik2, iv1, iv2, ic1, ic2, imode
-                                dK = calc_DKernel_mat_elem(indexes, KernelMat, EDFT, EQP, ELPH, MF_params, BSE_params)
+                                dK = calc_DKernel_mat_elem(
+                                    indexes, KernelMat, EDFT, EQP, ELPH, MF_params, BSE_params)
 
                                 if calc_IBL_way == False:
-                                    DKernel[imode, ik1, ic1, iv1, ik2, ic2, iv2] = A_bra * dK * A_ket
+                                    DKernel[imode, ik1, ic1, iv1, ik2,
+                                            ic2, iv2] = A_bra * dK * A_ket
                                 else:
-                                    DKernel[imode, ik1, ic1, iv1, ik2, ic2, iv2] = A_bra * dK[0] * A_ket
-                                    DKernel_IBL[imode, ik1, ic1, iv1, ik2, ic2, iv2] = A_bra * dK[1] * A_ket
+                                    DKernel[imode, ik1, ic1, iv1, ik2,
+                                            ic2, iv2] = A_bra * dK[0] * A_ket
+                                    DKernel_IBL[imode, ik1, ic1, iv1, ik2,
+                                                ic2, iv2] = A_bra * dK[1] * A_ket
 
                                 i_term += 1
                                 if i_term % interval_report == 0:
-                                    print(f'        {i_term} of {total_ite} calculated --------- {round(100*i_term/total_ite,1)} %')
-
+                                    print(
+                                        f'        {i_term} of {total_ite} calculated --------- {round(100*i_term/total_ite,1)} %')
 
     if calc_IBL_way == False:
         return DKernel
@@ -149,17 +151,16 @@ def calc_deriv_Kernel(KernelMat, EDFT, EQP, ELPH, Akcv, MF_params, BSE_params):
 
 
 def aux_matrix_elem(elph_cond, elph_val, Eqp_val, Eqp_cond, Edft_val, Edft_cond, MF_params, BSE_params, ikBSE_to_ikDFPT):
-
     """ Calculates auxiliar matrix elements to be used later in the forces matrix elements.
     Returns aux_cond_matrix, aux_val_matrix and
     aux_cond_matrix[imode, ik, ic1, ic2] = elph_cond[imode, ik, ic1, ic2] * deltaEqp / deltaEdft (if ic1 != ic2)
     aux_val_matrix[imode, ik, iv1, iv2]  = elph_val[imode, ik, iv1, iv2]  * deltaEqp / deltaEdft (if iv1 != iv2)
     If ic1 == ic2 (iv1 == iv2), then the matrix elements are just the elph coefficients"""
 
-    Nmodes   = MF_params.Nmodes
+    Nmodes = MF_params.Nmodes
     Nkpoints = BSE_params.Nkpoints_BSE
-    Ncbnds   = BSE_params.Ncbnds
-    Nvbnds   = BSE_params.Nvbnds
+    Ncbnds = BSE_params.Ncbnds
+    Nvbnds = BSE_params.Nvbnds
 
     Shape_cond = (Nmodes, Nkpoints, Ncbnds, Ncbnds)
     aux_cond_matrix = np.zeros(Shape_cond, dtype=np.complex64)
@@ -167,17 +168,16 @@ def aux_matrix_elem(elph_cond, elph_val, Eqp_val, Eqp_cond, Edft_val, Edft_cond,
     Shape_val = (Nmodes, Nkpoints, Nvbnds, Nvbnds)
     aux_val_matrix = np.zeros(Shape_val, dtype=np.complex64)
 
-
     for imode in range(Nmodes):
         for ik in range(Nkpoints):
 
-            ik_dfpt =  ikBSE_to_ikDFPT[ik]
+            ik_dfpt = ikBSE_to_ikDFPT[ik]
 
             if ik_dfpt != -1:
-            # remember that order of k points in DFPT file 
-            # is not always equal to the order in the eigenvecs file
-            # -1 means that the code did not find the corresponce 
-            # between the 2 kgrids
+                # remember that order of k points in DFPT file
+                # is not always equal to the order in the eigenvecs file
+                # -1 means that the code did not find the corresponce
+                # between the 2 kgrids
 
                 for ic1 in range(Ncbnds):
                     for ic2 in range(Ncbnds):
@@ -186,11 +186,12 @@ def aux_matrix_elem(elph_cond, elph_val, Eqp_val, Eqp_cond, Edft_val, Edft_cond,
 
                         if ic1 == ic2:
                             aux_cond_matrix[imode, ik, ic1, ic2] = elph
-                        
-                        elif abs(Edft_cond[ik, ic1] - Edft_cond[ik, ic2]) > TOL_DEG: 
+
+                        elif abs(Edft_cond[ik, ic1] - Edft_cond[ik, ic2]) > TOL_DEG:
                             deltaEqp = Eqp_cond[ik, ic1] - Eqp_cond[ik, ic2]
                             deltaEdft = Edft_cond[ik, ic1] - Edft_cond[ik, ic2]
-                            aux_cond_matrix[imode, ik, ic1, ic2] = elph * deltaEqp / deltaEdft
+                            aux_cond_matrix[imode, ik, ic1,
+                                            ic2] = elph * deltaEqp / deltaEdft
 
                 for iv1 in range(Nvbnds):
                     for iv2 in range(Nvbnds):
@@ -199,37 +200,39 @@ def aux_matrix_elem(elph_cond, elph_val, Eqp_val, Eqp_cond, Edft_val, Edft_cond,
 
                         if iv1 == iv2:
                             aux_val_matrix[imode, ik, iv1, iv2] = elph
-                        
-                        elif abs(Edft_val[ik, iv1] - Edft_val[ik, iv2]) > TOL_DEG: 
+
+                        elif abs(Edft_val[ik, iv1] - Edft_val[ik, iv2]) > TOL_DEG:
                             deltaEqp = Eqp_val[ik, iv1] - Eqp_val[ik, iv2]
                             deltaEdft = Edft_val[ik, iv1] - Edft_val[ik, iv2]
-                            aux_val_matrix[imode, ik, iv1, iv2] = elph * deltaEqp / deltaEdft
+                            aux_val_matrix[imode, ik, iv1,
+                                           iv2] = elph * deltaEqp / deltaEdft
 
     return aux_cond_matrix, aux_val_matrix
 
-def delta(i,j):
+
+def delta(i, j):
     """Dirac delta - TODO: check if python has a builtin function that does that."""
     if i == j:
         return 1.0
     else:
         return 0.0
 
-def dirac_delta_Edft(i,j, Edft, TOL_DEG):
+
+def dirac_delta_Edft(i, j, Edft, TOL_DEG):
     if abs(Edft[0, i] - Edft[0, j]) > TOL_DEG:
         return 1.0
     else:
         return 0.0
 
 
-def calc_Dkinect_matrix_elem(Akcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic2, iv1, iv2):
-
+def calc_Dkinect_matrix_elem(Akcv, Bkcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic2, iv1, iv2):
     """Calculates excited state force matrix elements."""
 
     # calculate matrix element imode, ik, ic1, ic2, iv1, iv2
     temp_cond = aux_cond_matrix[imode, ik, ic1, ic2] * delta(iv1, iv2)
     temp_val = aux_val_matrix[imode, ik, iv1, iv2] * delta(ic1, ic2)
 
-    tempA = Akcv[ik, ic1, iv1] * np.conj(Akcv[ik, ic2, iv2])
+    tempA = Akcv[ik, ic1, iv1] * np.conj(Bkcv[ik, ic2, iv2])
     Dkin = tempA * (temp_cond - temp_val)
 
     if report_RPA_data == True:
@@ -237,49 +240,51 @@ def calc_Dkinect_matrix_elem(Akcv, aux_cond_matrix, aux_val_matrix, imode, ik, i
         temp_text = f'{imode+1} {ik+1} {ic1+1} {ic2+1} {iv1+1} {iv2+1} {Dkin} {tempA} {temp_cond} {temp_val} \n'
         arq_RPA_data.write(temp_text)
         arq_RPA_data.close()
-    
+
     return Dkin
 
-def calc_Dkinect_matrix(Akcv, aux_cond_matrix, aux_val_matrix, MF_params, BSE_params):
+
+def calc_Dkinect_matrix(Akcv, Bkcv, aux_cond_matrix, aux_val_matrix, MF_params, BSE_params):
 
     print("\n\n     - Calculating RPA part")
 
-    Nmodes   = MF_params.Nmodes
+    Nmodes = MF_params.Nmodes
     Nkpoints = BSE_params.Nkpoints_BSE
-    Ncbnds   = BSE_params.Ncbnds
-    Nvbnds   = BSE_params.Nvbnds
+    Ncbnds = BSE_params.Ncbnds
+    Nvbnds = BSE_params.Nvbnds
 
     Shape = (Nmodes, Nkpoints, Ncbnds, Nvbnds, Nkpoints, Ncbnds, Nvbnds)
     DKinect = np.zeros(Shape, dtype=np.complex64)
 
     if report_RPA_data == True:
         arq_RPA_data = open('RPA_matrix_elements.dat', 'w')
-        arq_RPA_data.write('# mode ik ic1 ic2 iv1 iv2 F conj(Akc1v1)*Akc2v2 auxMatcond(c1,c2) auxMatval(v1,v2)\n')
+        arq_RPA_data.write(
+            '# mode ik ic1 ic2 iv1 iv2 F conj(Akc1v1)*Akc2v2 auxMatcond(c1,c2) auxMatval(v1,v2)\n')
         arq_RPA_data.close()
-
 
     if just_RPA_diag == True:
 
         # reporting
-        total_ite          = Nmodes*Nkpoints*Ncbnds*Nvbnds
-        interval_report    = max(int(np.sqrt(total_ite)), int(total_ite/20))
+        total_ite = Nmodes*Nkpoints*Ncbnds*Nvbnds
+        interval_report = max(int(np.sqrt(total_ite)), int(total_ite/20))
         i_term = 0
 
         print('Calculating just diag RPA force matrix elements')
         print(f'Total terms to be calculated : {total_ite}')
-        
+
         for imode in range(Nmodes):
             for ik in range(Nkpoints):
                 for ic1 in range(Ncbnds):
                     for iv1 in range(Nvbnds):
-                        temp = calc_Dkinect_matrix_elem(Akcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic1, iv1, iv1)
+                        temp = calc_Dkinect_matrix_elem(
+                            Akcv, Bkcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic1, iv1, iv1)
                         DKinect[imode, ik, ic1, iv1, ik, ic1, iv1] = temp
 
                         # reporting
                         i_term += 1
                         if i_term % interval_report == 0:
-                            print(f'{i_term} of {total_ite} calculated --------- {round(100*i_term/total_ite,1)} %')
-                        
+                            print(
+                                f'{i_term} of {total_ite} calculated --------- {round(100*i_term/total_ite,1)} %')
 
     else:
 
@@ -288,8 +293,8 @@ def calc_Dkinect_matrix(Akcv, aux_cond_matrix, aux_val_matrix, MF_params, BSE_pa
         if use_hermicity_F == False:
 
             # reporting
-            total_ite          = Nmodes*Nkpoints*(Ncbnds*Nvbnds)**2
-            interval_report    = max(int(np.sqrt(total_ite)), int(total_ite/20))
+            total_ite = Nmodes*Nkpoints*(Ncbnds*Nvbnds)**2
+            interval_report = max(int(np.sqrt(total_ite)), int(total_ite/20))
             i_term = 0
             print(f'Total terms to be calculated : {total_ite}')
 
@@ -299,13 +304,16 @@ def calc_Dkinect_matrix(Akcv, aux_cond_matrix, aux_val_matrix, MF_params, BSE_pa
                         for iv1 in range(Nvbnds):
                             for ic2 in range(Ncbnds):
                                 for iv2 in range(Nvbnds):
-                                    temp = calc_Dkinect_matrix_elem(Akcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic2, iv1, iv2)
-                                    DKinect[imode, ik, ic1, iv1, ik, ic2, iv2] = temp
+                                    temp = calc_Dkinect_matrix_elem(
+                                        Akcv, Bkcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic2, iv1, iv2)
+                                    DKinect[imode, ik, ic1, iv1,
+                                            ik, ic2, iv2] = temp
 
                                     # reporting
                                     i_term += 1
                                     if i_term % interval_report == 0:
-                                        print(f'{i_term} of {total_ite} calculated --------- {round(100*i_term/total_ite,1)} %')
+                                        print(
+                                            f'{i_term} of {total_ite} calculated --------- {round(100*i_term/total_ite,1)} %')
 
         # Creating a list of tuples with cond and val bands indexes
         # [(0,0), (0,1), (0,2), ... (Ncbnds-1, 0), (Ncbnds-1, 1), ..., (Ncbnds-1, Nvbnds-1)]
@@ -318,11 +326,13 @@ def calc_Dkinect_matrix(Akcv, aux_cond_matrix, aux_val_matrix, MF_params, BSE_pa
 
             print('Using "hermicity" of force matrix elements')
 
-            indexes_cv = [(icp, ivp) for ivp in range(Nvbnds) for icp in range(Ncbnds)]
+            indexes_cv = [(icp, ivp) for ivp in range(Nvbnds)
+                          for icp in range(Ncbnds)]
 
             # reporting
-            total_ite          = int(Nmodes*Nkpoints*len(indexes_cv)*(len(indexes_cv)-1) / 2)
-            interval_report    = max(int(np.sqrt(total_ite)), int(total_ite/20))
+            total_ite = int(Nmodes*Nkpoints*len(indexes_cv)
+                            * (len(indexes_cv)-1) / 2)
+            interval_report = max(int(np.sqrt(total_ite)), int(total_ite/20))
             i_term = 0
             print(f'Total terms to be calculated : {total_ite}')
 
@@ -332,23 +342,27 @@ def calc_Dkinect_matrix(Akcv, aux_cond_matrix, aux_val_matrix, MF_params, BSE_pa
                         # diagonal term (cv,cv)
                         ic1, iv1 = indexes_cv[ind_cv1]
 
-                        temp = calc_Dkinect_matrix_elem(Akcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic1, iv1, iv1)
+                        temp = calc_Dkinect_matrix_elem(
+                            Akcv, Bkcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic1, iv1, iv1)
                         DKinect[imode, ik, ic1, iv1, ik, ic1, iv1] = temp
 
                         # Now get offdiag
-                        for ind_cv2 in range(ind_cv1+1, len(indexes_cv)): # don't get it repeated
+                        # don't get it repeated
+                        for ind_cv2 in range(ind_cv1+1, len(indexes_cv)):
 
                             ic2, iv2 = indexes_cv[ind_cv2]
-                            
-                            temp = calc_Dkinect_matrix_elem(Akcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic2, iv1, iv2)
+
+                            temp = calc_Dkinect_matrix_elem(
+                                Akcv, Bkcv, aux_cond_matrix, aux_val_matrix, imode, ik, ic1, ic2, iv1, iv2)
                             DKinect[imode, ik, ic1, iv1, ik, ic2, iv2] = temp
-                            DKinect[imode, ik, ic2, iv2, ik, ic1, iv1] = np.conj(temp)
+                            DKinect[imode, ik, ic2, iv2, ik,
+                                    ic1, iv1] = np.conj(temp)
 
                             # reporting
                             i_term += 1
                             if i_term % interval_report == 0:
-                                print(f'       {i_term} of {int(total_ite)} calculated --------- {round(100*i_term/total_ite,1)} %')                    
-
+                                print(
+                                    f'       {i_term} of {int(total_ite)} calculated --------- {round(100*i_term/total_ite,1)} %')
 
     if report_RPA_data == True:
         print(f'RPA matrix elements written in file: RPA_matrix_elements.dat')
