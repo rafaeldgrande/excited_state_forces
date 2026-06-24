e3c7dfdc6dbd683167395c44e3f21b75219b52cc
Rafael Del Grande <rafaeldgrande@gmail.com>
2026-06-24 11:05:54 -0700

    removed qe_interface_m.py

--- main/qe_interface_m.py
+++
@@ -1,4 +0,0 @@
-# This file is no longer used.
-# The QE XML el-ph reading workflow has been replaced by the pre-interpolated
-# elph_fine.h5 file produced by interpolate_elph_bgw.py.
-# This file can be safely deleted.
